8b41b35bd94d4ef964dfff7c575a514a4ce4594a
Vikram Singh Saini <sspl.vssaini@gmail.com>
2021-03-03 06:40:06 +0530

    Add project files.

---
+++ AD Groups/FrmMain.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ADGroups
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDomain = new System.Windows.Forms.TextBox();
+            this.btnGetGroups = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.bgWorkerGroups = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Domain:";
+            // 
+            // txtDomain
+            // 
+            this.txtDomain.Location = new System.Drawing.Point(70, 12);
+            this.txtDomain.Name = "txtDomain";
+            this.txtDomain.Size = new System.Drawing.Size(394, 23);
+            this.txtDomain.TabIndex = 1;
+            this.txtDomain.Text = "domain.com";
+            // 
+            // btnGetGroups
+            // 
+            this.btnGetGroups.Location = new System.Drawing.Point(375, 50);
+            this.btnGetGroups.Name = "btnGetGroups";
+            this.btnGetGroups.Size = new System.Drawing.Size(89, 23);
+            this.btnGetGroups.TabIndex = 2;
+            this.btnGetGroups.Text = "Get Groups";
+            this.btnGetGroups.UseVisualStyleBackColor = true;
+            this.btnGetGroups.Click += new System.EventHandler(this.btnGetGroups_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(15, 79);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.Size = new System.Drawing.Size(449, 228);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 322);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(476, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(188, 17);
+            this.lblStatus.Text = "Developed by - Vikram Singh Saini";
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            this.lblStatus.MouseLeave += new System.EventHandler(this.lblStatus_MouseLeave);
+            this.lblStatus.MouseHover += new System.EventHandler(this.lblStatus_MouseHover);
+            // 
+            // bgWorkerGroups
+            // 
+            this.bgWorkerGroups.WorkerReportsProgress = true;
+            this.bgWorkerGroups.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorkerGroups_DoWork);
+            this.bgWorkerGroups.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgWorkerGroups_ProgressChanged);
+            this.bgWorkerGroups.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorkerGroups_RunWorkerCompleted);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 344);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnGetGroups);
+            this.Controls.Add(this.txtDomain);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(492, 383);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(492, 383);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AD Groups";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDomain;
+        private System.Windows.Forms.Button btnGetGroups;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.ComponentModel.BackgroundWorker bgWorkerGroups;
+    }
+}
+
---
+++ AD Groups/FrmMain.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ADGroups
+{
+    public partial class FrmMain : Form
+    {
+        private int _groupsRetrieved;
+
+        private List<Group> _groups;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetGroups_Click(object sender, EventArgs e)
+        {
+            // Reset all to default
+            dataGridView.DataSource = null;
+            _groupsRetrieved = 0;
+            btnGetGroups.Enabled = false;
+
+            var domain = txtDomain.Text.Trim();
+
+            if (!bgWorkerGroups.IsBusy)
+                bgWorkerGroups.RunWorkerAsync(domain);
+        }
+
+        private void bgWorkerGroups_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var domain = (string)e.Argument;
+
+            if (_groups == null)
+                _groups = new List<Group>();
+
+            // Create principal context
+            var principalContext = new PrincipalContext(ContextType.Domain, domain);
+            //principalContext.ValidateCredentials("Administrator", "Pass99");
+
+            using (principalContext)
+            {
+                bgWorkerGroups.ReportProgress(0, "Please wait! Searching all groups...");
+
+                using (var groupPrincipal = new GroupPrincipal(principalContext))
+                {
+                    using (var principalSearcher = new PrincipalSearcher(groupPrincipal))
+                    {
+                        var principals = principalSearcher.FindAll().OfType<GroupPrincipal>();
+                        var groupPrincipals = principals as GroupPrincipal[] ?? principals.ToArray();
+
+                        foreach (var gp in groupPrincipals)
+                        {
+                            // Check for duplicates and change friendly name accordingly
+                            var dupCount = _groups.Count(@group => @group.DistinguishedName.Equals(gp.DistinguishedName));
+
+                            if (dupCount.Equals(0))
+                            {
+                                _groups.Add(new Group(gp.SamAccountName, gp.DistinguishedName));
+                            }
+                            else
+                            {
+                                var samAccountName = string.Format("{0}{1}", gp.SamAccountName, dupCount);
+                                _groups.Add(new Group(samAccountName, gp.DistinguishedName));
+                            }
+
+                            _groupsRetrieved++;
+                            bgWorkerGroups.ReportProgress(0, string.Format("Retrieved group '{0}' ({1}/{2})", gp.Name, _groupsRetrieved, groupPrincipals.Count()));
+                        }
+                    }
+                }
+
+                // Sort and assign end results
+                _groups.Sort((a, b) => String.Compare(a.FriendlyName, b.FriendlyName, StringComparison.OrdinalIgnoreCase));
+                e.Result = _groups;
+            }
+        }
+
+        private void bgWorkerGroups_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            lblStatus.Text = (string)e.UserState;
+        }
+
+        private void bgWorkerGroups_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                lblStatus.Text = "Error was reported.";
+                MessageBox.Show("Error:- " + e.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (_groupsRetrieved > 0)
+                {
+                    var message = string.Format(_groupsRetrieved.Equals(1) ? "{0} group was imported successfully!" : "{0} groups were imported successfully!", _groupsRetrieved);
+                    lblStatus.Text = message;
+
+                    dataGridView.DataSource = e.Result;
+                }
+                else
+                    lblStatus.Text = "No group found!";
+            }
+
+            btnGetGroups.Enabled = true;
+        }
+
+        private void lblStatus_MouseHover(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Hand;
+        }
+
+        private void lblStatus_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://lnkd.in/bJ3eyHY");
+        }
+    }
+
+    /// <summary>
+    /// Represents Group of AD.
+    /// </summary>
+    public class Group
+    {
+        public Group(string samName, string dn)
+        {
+            FriendlyName = samName;
+            DistinguishedName = dn;
+        }
+
+        public string FriendlyName { get; set; }
+        public string DistinguishedName { get; set; }
+    }
+}
